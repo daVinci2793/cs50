ce3bc7c17889d964152d1504475d2bfe503dfe1f
daVinci2793 <49510984+daVinci2793@users.noreply.github.com>
2019-04-22 14:30:05 -0700

    Update vigenere.c

--- vigenere.c
+++ vigenere.c
@@ -20,131 +20,69 @@ int main(int argc, string argv[])
             return 1;
         }
     }
-    
+    string t = get_string("plaintext: ");   
     string s = argv[1];
-    int cipher[strlen(s)];
-    int b = 0;
-    for (int i = 0; i < strlen(s); i++)
-    {           
-        if (isupper(s[i]))
-        {
-            b = (int) s[i];
-            cipher[i] = b;
-            cipher[i] -= 65;
-        }
-        else
-        {
-            b = (int) s[i];
-            cipher[i] = b;
-            cipher[i] -= 97;
-        }                   
-    }
-
-    string t = get_string("plaintext: ");
-    int message[strlen(t)];
+    int n = strlen(s);
+    int m = strlen(t);     
+    int message[m];
     int c = 0;
-    for (int i = 0; i < strlen(t); i++)
+    for (int i = 0; i < m; i++)
     {        
-        if (isupper(t[i]))
-        {
-            c = (int) t[i];
-            message[i] = c;
-            message[i] -= 65;
-            
-        }
-        else if (isblank(t[i]))
-        {
-            c = (int) t[i];
-            message[i] = c;
-            message[i] -= 32;
-        }
-        else
-        {
-            c = (int) t[i];
-            message[i] = c;
-            message[i] -= 97;
-        }                   
+        c = (int) t[i];
+        message[i] = c;           
     }
-
-    int key[strlen(t)];
-    if (strlen(s) < strlen(t))
-    {
-        for (int i = 0, n = strlen(t), j = 0; i < n; i ++, j++)
+    
+    int cipher[n];
+    int b = 0;
+    int key[m];
+    for (int i = 0, j = 0; i < m; i ++, j++)    
+    {           
+        if (j == n || j < 0)
         {
-            if (j == strlen(s))
-            {
-                j = 0;
-            }
-            key[i] = cipher[j];
-            printf("%d\n", key[i]);
+            j = 0;
         }
+        if (!isalpha(t[i]))
+        {
+            j = (j - 1);
+        }  
+        b = (int) s[j];
+        cipher[j] = (b - 97);
+        key[i] = tolower(cipher[j]);
     }
     printf("ciphertext: ");
 
-    int cryptograph[strlen(t)];
-    char output[strlen(t)];    
-    if (strlen(s) < strlen(t))
+    int cryptograph[m];
+    char output[m];
+    char d = 0;
     {
-        for (int i = 0, n = strlen(t); i < n; i++)
+        for (int i = 0; i < m; i++)
         {
             if (isupper(t[i]))
             {
-                cryptograph[i] = ((message[i] + key[i]) % 26) + 65;
-                char d = (char) cryptograph[i];
+                cryptograph[i] = ((message[i] - 65 + key[i]) % 26) + 65;
+                d = (char) cryptograph[i];
                 output[i] = d;
             }
             else if (ispunct(t[i]))
             {
                 cryptograph[i] = t[i];
-                char d = (char) cryptograph[i];
+                d = (char) cryptograph[i];
                 output[i] = d;
             }
             else if (isblank(t[i]))
             {
                 cryptograph[i] = 32;
-                char d = (char) cryptograph[i];
+                d = (char) cryptograph[i];
                 output[i] = d;
             }
             else
             {
-                cryptograph[i] = ((message[i] + key[i]) % 26) + 97;
-                char d = (char) cryptograph[i];
+                cryptograph[i] = ((message[i] - 97 + key[i]) % 26) + 97;
+                d = (char) cryptograph[i];
                 output[i] = d;
             }  
             printf("%c", output[i]);
         }
         printf("\n");
-    }
-    else
-    {
-        for (int i = 0, n = strlen(t); i < n; i++)
-        {
-            if (isupper(t[i]))
-            {
-                cryptograph[i] = ((message[i] + cipher[i]) % 26) + 65;
-                char d = (char) cryptograph[i];
-                output[i] = d;
-            }
-            else if (ispunct(t[i]))
-            {
-                cryptograph[i] = t[i];
-                char d = (char) cryptograph[i];
-                output[i] = d;
-            }
-            else if (isblank(t[i]))
-            {
-                cryptograph[i] = 32;
-                char d = (char) cryptograph[i];
-                output[i] = d;
-            }
-            else
-            {
-                cryptograph[i] = ((message[i] + cipher[i]) % 26) + 97;
-                char d = (char) cryptograph[i];
-                output[i] = d;
-            }  
-            printf("%c", output[i]);
-        }
-        printf("\n");
-    }
+       }
 }
